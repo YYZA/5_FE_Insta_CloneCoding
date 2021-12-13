21eda8197827faa7f32ee2fafec5ca586655899a
undriedspring <hansam5615@gmail.com>
2021-12-13 20:59:28 +0900

    213am_1213_21h

--- src/components/UserNameTag.jsx
+++ src/components/UserNameTag.jsx
@@ -6,7 +6,7 @@ const UserNameTag = () => {
     <>
       <container>
         <Grid is_flex width="auto">
-          <Image size="50" src="https://e7.pngegg.com/pngimages/914/758/png-clipart-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper.png"></Image>
+          <Image size="45" src="https://e7.pngegg.com/pngimages/914/758/png-clipart-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper.png"></Image>
           <Text>dlgkstoa</Text>
         </Grid>
       </container>
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -8,14 +8,15 @@ const Main = () => {
   return (
     <>
       <div className="background">
-        <div className>
-          <div className="container">
-            <PostCard></PostCard>
-          </div>
-          <Grid>
-            <Footer></Footer>
-          </Grid>
+        <div className="container">
+          <PostCard></PostCard>
         </div>
+        <Grid>
+          <Footer></Footer>
+        </Grid>
+        <Grid>
+          <Image src="https://www.hidomin.com/news/photo/202105/453232_224470_4025.jpg" />
+        </Grid>
       </div>
     </>
   )
