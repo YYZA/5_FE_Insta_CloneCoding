18d8a5d4691c43b4cce87793f7bac2427319f588
YYZA <sacoraa@naver.com>
2021-12-13 20:32:58 +0900

    login,profile,signup CSS

--- src/page/Signin.jsx
+++ src/page/Signin.jsx
@@ -1,7 +1,83 @@
 import React from "react";
+import styled from "styled-components";
+import Grid from "../elements/Grid";
+import { history } from "../redux/configureStore";
 
 const Siginin = () => {
-  return <React.Fragment>asdsad</React.Fragment>;
+  return (
+    <div
+      style={{
+        background:
+          "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYrHPu9t5m4ShcjEtJhoMJIkYZ64fl8q2veA&usqp=CAU)",
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+        margin: "0px",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Container>
+        <Logo>Instartgram</Logo>
+        <Input type="text" placeholder="이메일을 입력해주세요"></Input>
+        <Input type="text" placeholder="비밀번호를 입력해주세요"></Input>
+        <LoginBtn>로그인</LoginBtn>
+        <LoginBtn
+          onClick={() => {
+            history.push("/signup");
+          }}
+        >
+          회원가입
+        </LoginBtn>
+      </Container>
+    </div>
+  );
 };
 
 export default Siginin;
+const Logo = styled.div`
+  margin-bottom: 50px;
+  font-size: 50px;
+  font-family: "Lobster", cursive;
+`;
+
+const Container = styled.div`
+  height: 490px;
+  width: 450px;
+  padding: 150px 0 0 0;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+    rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+    rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px,
+    rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px,
+    rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
+  margin: 0px auto;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  outline: none;
+  height: 45px;
+  width: 300px;
+  margin-bottom: 10px;
+  background-color: #fafafa;
+  border: #efefef 1px solid;
+  border-radius: 6px;
+  padding-left: 10px;
+`;
+
+const LoginBtn = styled.button`
+  width: 315px;
+  height: 35px;
+  border-radius: 6px;
+  border: white;
+  margin-bottom: 10px;
+  background-color: #0095f6;
+  color: white;
+  font-size: 17px;
+  cursor: pointer;
+`;
--- src/page/UserProfile.jsx
+++ src/page/UserProfile.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import styled from "styled-components";
+import Text from "../elements/Text";
+import Header from "../components/Header";
 
-const UserProfile = () => {
-  return null;
+const UserProfile = (props) => {
+  return (
+    <div
+      style={{
+        background:
+          "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYrHPu9t5m4ShcjEtJhoMJIkYZ64fl8q2veA&usqp=CAU)",
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+        margin: "0px",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Container>
+        <Header />
+        <Circle src={props.user_image}></Circle>
+        <Text weight="600" size="24px">
+          {props.nickname}
+        </Text>
+        <Rectangle src={props.user_image}></Rectangle>
+      </Container>
+    </div>
+  );
+};
+
+UserProfile.defaultProps = {
+  nickname: "YYZAS",
+  user_image: "https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927",
 };
 
 export default UserProfile;
+const Rectangle = styled.img`
+  width:"200px"
+  height:"200px"
+  
+`;
+
+const Circle = styled.img`
+  border-radius: 50%;
+  margin: 100px 0 0 0;
+  width: 30%;
+`;
+
+const Container = styled.div`
+  height: 690px;
+  width: 450px;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+    rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+    rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px,
+    rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px,
+    rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
+  margin: 0px auto;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
