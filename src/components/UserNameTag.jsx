65a9deba1a55297a0f955eec6d6bb8efcd567249
undriedspring <hansam5615@gmail.com>
2021-12-14 16:06:37 +0900

    213am_1214_16h

--- src/components/UserNameTag.jsx
+++ src/components/UserNameTag.jsx
@@ -4,7 +4,7 @@ import { Grid, Image, Text } from '../elements'
 const UserNameTag = () => {
   return (
     <>
-      <div className="NameTagButton">
+      <div>
         <Grid is_flex width="auto">
           <Image size="45" src="https://e7.pngegg.com/pngimages/914/758/png-clipart-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper.png" />
           <Text fontWeight="bold">dlwlrma</Text>
