2ce55f9cdd8c57a7ec6694c6657a0c0713687da6
YYZA <sacoraa@naver.com>
2021-12-16 09:30:16 +0900

    Profile Page CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,62 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import "../css/Main.css";
 import { useSelector } from "react-redux";
-import Text from "../elements/Text";
-import IconButton from "@mui/material/IconButton";
-import AddIcon from "@mui/icons-material/Add";
-import MenuIcon from "@mui/icons-material/Menu";
 import { getCookie } from "../shared/Cookie";
 import { history } from "../redux/configureStore";
-let cookie = getCookie("x_auth");
+import HomeIcon from "@mui/icons-material/Home";
+import { FiSend } from "react-icons/fi";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import InstaLogo from "../shared/icon/Instagram_logo.svg";
 
 const Header = (props) => {
-  const is_login = useSelector((state) => state.user.is_login);
   return (
     <>
-      {cookie || is_login ? (
-        <HeaderDiv>
-          <Logo>Instargram</Logo>
-          <div style={{ marginRight: "10px" }}>
-            <IconButton
-              onClick={() => {
-                history.push("/PostAdd");
-              }}
-              aria-label="delete"
-            >
-              <AddIcon />
-            </IconButton>
-            <IconButton
-              onClick={() => {
-                history.push("/profile");
-              }}
-              aria-label="delete"
-            >
-              <MenuIcon />
-            </IconButton>
+      <div className="Background">
+        <div className="MainLayout">
+          <div className="MainHeader">
+            <div className="HeaderSection">
+              <img className="InstagramLogo" src={InstaLogo} />
+              <input placeholder=" 🔍  검색" className="HeaderSearchBar" />
+              <HomeIcon
+                className="HistoryMainButton"
+                fontSize="5"
+                onClick={() => history.push("/main")}
+              />
+              <FiSend className="HeaderShareButton" fontSize="5" />
+              <AddBoxOutlinedIcon
+                className="PostAddButton"
+                fontSize="5"
+                onClick={() => history.push("/postAdd")}
+              />
+              <ExploreOutlinedIcon className="CompassButton" fontSize="5" />
+              <FavoriteBorderOutlinedIcon
+                className="HeaderLikeButton"
+                fontSize="5"
+              />
+              <AccountCircleOutlinedIcon
+                className="UserPageButton"
+                fontSize="5"
+              />
+            </div>
           </div>
-        </HeaderDiv>
-      ) : (
-        <HeaderDiv>
-          <Logo>Instargram</Logo>
-        </HeaderDiv>
-      )}
+        </div>
+      </div>
     </>
   );
 };
 
-const Logo = styled.div`
-  font-size: 20px;
-  font-family: "Lobster", cursive;
-  margin-left: 20px;
-`;
-
-const HeaderDiv = styled.div`
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 450px;
-  height: 50px;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
-`;
 export default Header;
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,64 +1,41 @@
-import React from 'react'
-import '../css/Main.css'
-import Modal from 'react-modal'
-import { useDispatch, useSelector } from 'react-redux'
-import { history } from '../redux/configureStore'
-import Footer from '../components/Footer'
-import PostCard from '../components/PostCard'
-import { Grid, Image, Text } from '../elements'
-import HomeIcon from '@mui/icons-material/Home'
-import { FiSend } from 'react-icons/fi'
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined'
-import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined'
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
-import InstaLogo from '../shared/icon/Instagram_logo.svg'
+import React from "react";
+import "../css/Main.css";
+import Modal from "react-modal";
+import { useDispatch, useSelector } from "react-redux";
+import Footer from "../components/Footer";
+import PostCard from "../components/PostCard";
+import { Grid, Image, Text } from "../elements";
+import Header from "../components/Header";
 
 const Main = () => {
-  const dispatch = useDispatch()
-  const post_data = useSelector((state) => state.post.list)
+  const dispatch = useDispatch();
+  const post_data = useSelector((state) => state.post.list);
 
   return (
     <>
-      <div className="Background">
-        <div className="MainLayout">
-          <div className="MainHeader">
-            <div className="HeaderSection">
-              <img className="InstagramLogo" src={InstaLogo} />
-              <input placeholder=" 🔍  검색" className="HeaderSearchBar" />
-              <HomeIcon className="HistoryMainButton" fontSize="5" onClick={() => history.push('/main')} />
-              <FiSend className="HeaderShareButton" fontSize="5" />
-              <AddBoxOutlinedIcon className="PostAddButton" fontSize="5" onClick={() => history.push('/postAdd')} />
-              <ExploreOutlinedIcon className="CompassButton" fontSize="5" />
-              <FavoriteBorderOutlinedIcon className="HeaderLikeButton" fontSize="5" />
-              <AccountCircleOutlinedIcon className="UserPageButton" fontSize="5" />
-            </div>
-          </div>
-
-          <div className="BodyLayout">
-            <div className="MainFooterLayout">
-              <Footer />
-            </div>
-            <div className="MainPostLayout">
-              {/* <div className="MyFollowing">내가 팔로우 하는 사람</div> */}
-              {post_data.map((p, idx) => {
-                return (
-                  <Grid key={idx} margin="0px">
-                    <PostCard {...p} />
-                  </Grid>
-                )
-              })}
-              <PostCard></PostCard>
-              <PostCard></PostCard>
-              <PostCard></PostCard>
-              <PostCard></PostCard>
-              <PostCard></PostCard>
-            </div>
-          </div>
+      <Header />
+      <div className="BodyLayout">
+        <div className="MainFooterLayout">
+          <Footer />
+        </div>
+        <div className="MainPostLayout">
+          {/* <div className="MyFollowing">내가 팔로우 하는 사람</div> */}
+          {post_data.map((p, idx) => {
+            return (
+              <Grid key={idx} margin="0px">
+                <PostCard {...p} />
+              </Grid>
+            );
+          })}
+          <PostCard></PostCard>
+          <PostCard></PostCard>
+          <PostCard></PostCard>
+          <PostCard></PostCard>
+          <PostCard></PostCard>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
