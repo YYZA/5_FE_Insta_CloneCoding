133709e5f2bb86f159e12754627074284d54cc6c
undriedspring <hansam5615@gmail.com>
2021-12-18 15:50:26 +0900

    1218_16h

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,43 +13,28 @@ const Footer = (props) => {
   const nickname = postList.nickname
   const userId = postList.userId
 
+  var win = window.open('/') // 새탭에서 열림
   console.log(postList)
   const members = [
     {
-      src: 'https://kimkong2.s3.ap-northeast-2.amazonaws.com/avatar-dQCYs4n7O99ksXuBIe33-7i6cgyRhMHo2yv48rq19-yFpcQh7UcvdChVN8WvIW-iDeY4KaGvVDVSxCv46Bg-gOLxmc6a6kx5l0AM6k1f-qU1toWj69nUqJJ6wJjRx.png',
-      nickname: 'Jun',
-      blog: 'https://www.linkedin.com/in/seungjunkim0906/',
-      github: 'https://github.com/Seungjun0906',
+      src: 'https://5-instagramclonecoding.s3.ap-northeast-2.amazonaws.com/Samsunged.png',
+      nickname: '이건희',
+      github: 'https://github.com/IsthisLee',
     },
     {
-      src: 'https://kimkong2.s3.ap-northeast-2.amazonaws.com/avatar-sFQorI7AcjL4R4gtgerp-trLwpPTwirxVyPPaijwz.png',
-      nickname: 'StreetJae1',
-      blog: 'https://velog.io/@kiljw316',
-      github: 'https://github.com/kiljw316',
+      src: 'https://5-instagramclonecoding.s3.ap-northeast-2.amazonaws.com/Hyungjin.png',
+      nickname: '김형진',
+      github: 'https://github.com/KIMHYEONGJIN5925',
     },
     {
-      src: 'https://kimkong2.s3.ap-northeast-2.amazonaws.com/avatar-Qg3LanCVBxmnw9mLCfDC-8hCSfYIK6RvpToNgMJNB-TChgCSU6xSHuNmR2PI7H-yFpcQh7UcvdChVN8WvIW-h7sHVeDMuiqz5RrUBjQS-pWUQMKm2XzZjirTcVib7-TniAAPqhvIe0Zu0INEAl-eAEJrpPbgFVpm4tIqUdp.png',
-      nickname: 'mincho',
-      blog: 'https://luce-sicut-stella.tistory.com/',
-      github: 'https://github.com/mintsid',
+      src: 'https://5-instagramclonecoding.s3.ap-northeast-2.amazonaws.com/Dragoncastle.png',
+      nickname: '김용성',
+      github: 'https://github.com/YYZA',
     },
     {
-      src: 'https://kimkong2.s3.ap-northeast-2.amazonaws.com/avatar-8hCSfYIK6RvpToNgMJNB-Gexsm2gfIWciOSX1CLFB-yFpcQh7UcvdChVN8WvIW-xvQ1RrDVAEmBqo3T79RY-3Gg4mhaXMTgvjYYAfDpi-MHNdhjFVLOGVF3pTfLqP.png',
-      nickname: '213am',
-      blog: 'https://velog.io/@undriedspring',
+      src: 'https://5-instagramclonecoding.s3.ap-northeast-2.amazonaws.com/213am.png',
+      nickname: '이한샘',
       github: 'https://github.com/undriedspring',
-    },
-    {
-      src: 'https://kimkong2.s3.ap-northeast-2.amazonaws.com/avatar-C0ykfrlDx7AkQsLyLcNS-EDEmBTn7nj52wSnvBAmp-bjYIM3RJKVm2mwEI3BrQ-uHozYGHbW35roEHKzF98-WaMM8u1pW2GyKfBCGCZp-CgjGnJ2FTTfWiE3tf7Uj-WW1GTt4cFIfI7aG4zd1o-7EyjSDVlNscTAlYLSHV1.png',
-      nickname: 'jann_2',
-      blog: 'https://jann2codingdiary.tistory.com/manage/posts/',
-      github: 'https://github.com/jawoon816',
-    },
-    {
-      src: 'https://kimkong2.s3.ap-northeast-2.amazonaws.com/avatar-KqTqvfFl86GbTsCRv5AY-PRoVp9KM6WZwvLkGEO8V-I7vkRzijl8vBGm5LRwQT-cCWIF8nGqKTfCCY0iXWl-py34w1ylAbJPYocLYKkq-IXv6QOFQKcNdxBtiXAXI-0hw2J9tExTUUG8aNnNvz-4YbAAJpkVityygIfsOkk.png',
-      nickname: 'zubetcha',
-      blog: 'https://zubetcha.tistory.com/',
-      github: 'https://github.com/zubetcha',
     },
   ]
 
@@ -61,8 +46,8 @@ const Footer = (props) => {
             <Grid is_flex width="200px">
               <Image src={BasicProfile} shape="circle" size="55"></Image>
               <div className="MyAccountInfo">
-                <text className="MyAccountNickname">{nickname}</text>
-                <text className="MyName">{userId}</text>
+                <text className="MyAccountNickname">UserNickname</text>
+                <text className="MyName">UserName</text>
               </div>
             </Grid>
             <div className="ChangeAccountSection">
@@ -79,11 +64,15 @@ const Footer = (props) => {
             <div className="FooterContainer">
               <div className="NameTag">
                 <Image src={member.src} shape="circle" size="30"></Image>
-                <Text color="#333" size="14px" margin="0">
+                <Text color="#333" size="13px" margin="0">
                   {member.nickname}
                 </Text>
               </div>
-              <div className="FollowButton">팔로우</div>
+              <div>
+                <a className="FollowButton" href={member.github} target="_blank">
+                  팔로우
+                </a>
+              </div>
             </div>
           )
         })}
