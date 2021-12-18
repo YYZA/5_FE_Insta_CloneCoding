efc437aefd04947d812a151c131e0c7bcfa0e7ee
YYZA <sacoraa@naver.com>
2021-12-18 15:48:19 +0900

    Profile 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import '../css/Header.css'
-import { useSelector } from 'react-redux'
-import { getCookie } from '../shared/Cookie'
-import { history } from '../redux/configureStore'
-import $ from 'jquery'
-import HomeIcon from '@mui/icons-material/Home'
-import { FiSend } from 'react-icons/fi'
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined'
-import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined'
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
-import InstaLogo from '../shared/icon/Instagram_logo.svg'
+import React from "react";
+import styled from "styled-components";
+import "../css/Header.css";
+import { useSelector } from "react-redux";
+import { getCookie } from "../shared/Cookie";
+import { history } from "../redux/configureStore";
+import $ from "jquery";
+import HomeIcon from "@mui/icons-material/Home";
+import { FiSend } from "react-icons/fi";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import InstaLogo from "../shared/icon/Instagram_logo.svg";
+import { userActions } from "../redux/modules/user";
+import { useDispatch } from "react-redux";
 
 const Header = (props) => {
+  const dispatch = useDispatch();
   function wrapWindowLayerMask() {
     //화면의 높이와 너비를 구한다.
-    var maskHeight = $(document).height()
-    var maskWidth = $(window).width()
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
 
     //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채운다.
-    $('#WindowLayerMask').css({ width: maskWidth, height: maskHeight })
+    $("#WindowLayerMask").css({ width: maskWidth, height: maskHeight });
 
     //애니메이션 효과 - 일단 1초동안 까맣게 됐다가 80% 불투명도로 간다.
-    $('#WindowLayerMask').fadeIn(1000)
-    $('#WindowLayerMask').fadeTo('slow', 0.8)
+    $("#WindowLayerMask").fadeIn(1000);
+    $("#WindowLayerMask").fadeTo("slow", 0.8);
 
     //윈도우 같은 거 띄운다.
-    $('.window').show()
+    $(".window").show();
   }
 
   return (
@@ -35,18 +38,38 @@ const Header = (props) => {
       <div id="WindowLayerMask" />
       <div className="MainHeader">
         <div className="HeaderSection">
-          <img className="InstagramLogo" size="5" src={InstaLogo} onClick={() => history.push('/main')} />
+          <img
+            className="InstagramLogo"
+            size="5"
+            src={InstaLogo}
+            onClick={() => history.push("/main")}
+          />
           <input placeholder=" 🔍  검색" className="HeaderSearchBar" />
-          <HomeIcon className="HistoryMainButton" fontSize="5" onClick={() => history.push('/main')} />
+          <HomeIcon
+            className="HistoryMainButton"
+            fontSize="5"
+            onClick={() => history.push("/main")}
+          />
           <FiSend className="HeaderShareButton" fontSize="5" />
-          <AddBoxOutlinedIcon className="PostAddButton" fontSize="5" onClick={() => history.push('/main/postAdd')} />
+          <AddBoxOutlinedIcon
+            className="PostAddButton"
+            fontSize="5"
+            onClick={() => history.push("/main/postAdd")}
+          />
           <ExploreOutlinedIcon className="CompassButton" fontSize="5" />
-          <FavoriteBorderOutlinedIcon className="HeaderLikeButton" fontSize="5" />
-          <AccountCircleOutlinedIcon className="UserPageButton" fontSize="5" onClick={() => history.push('/profile')} />
+          <FavoriteBorderOutlinedIcon
+            className="HeaderLikeButton"
+            fontSize="5"
+          />
+          <AccountCircleOutlinedIcon
+            className="UserPageButton"
+            fontSize="5"
+            onClick={() => dispatch(userActions.previewDB())}
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,30 +1,31 @@
-import produce from 'immer'
-import { createAction, handleActions } from 'redux-actions'
-import { getCookie } from '../../shared/Cookie'
-import axios from 'axios'
+import produce from "immer";
+import { createAction, handleActions } from "redux-actions";
+import { getCookie } from "../../shared/Cookie";
+import axios from "axios";
 
 const initialState = {
   is_login: false,
   user: [],
   img: [],
-}
+};
 
-const SET_USER = 'SET_USER'
-const PREVIEW = 'PREVIEW'
+const SET_USER = "SET_USER";
+const PREVIEW = "PREVIEW";
 
-const setUser = createAction(SET_USER, (user) => ({ user }))
-const preview = createAction(PREVIEW, (img) => ({ img }))
+const setUser = createAction(SET_USER, (user) => ({ user }));
+const preview = createAction(PREVIEW, (img) => ({ img }));
 
 const previewDB = () => {
-  const cookie = getCookie('authCookie')
+  const cookie = getCookie("authCookie");
   return async function (dispatch, getState, { history }) {
     await axios
-      .get('http://13.125.149.78/api/me', {
+      .get("http://13.125.149.78/api/me", {
         headers: {
           Authorization: cookie,
         },
       })
       .then((res) => {
+        console.log(res);
         axios
           .get(`http://13.125.149.78/api/userprofile/${res.data.userId}`, {
             headers: {
@@ -32,33 +33,33 @@ const previewDB = () => {
             },
           })
           .then((res) => {
-            console.log(res)
-            dispatch(preview(res.data.userPosts))
-            history.push('/profile')
-          })
-      })
-  }
-}
+            console.log(res);
+            dispatch(preview(res.data.userPosts));
+            history.push("/profile");
+          });
+      });
+  };
+};
 
 export default handleActions(
   {
     [SET_USER]: (state, action) =>
       produce(state, (draft) => {
-        draft.is_login = true
-        draft.user = action.payload.user
+        draft.is_login = true;
+        draft.user = action.payload.user;
       }),
     [PREVIEW]: (state, action) =>
       produce(state, (draft) => {
-        console.log(action.payload)
-        draft.img = action.payload
+        console.log(action.payload);
+        draft.img = action.payload;
       }),
   },
   initialState
-)
+);
 
 const userActions = {
   setUser,
   preview,
   previewDB,
-}
-export { userActions }
+};
+export { userActions };
